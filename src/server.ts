1780bbea135ca910329e509e17a3a42f17b4bae2
Nazmul Hasan Shadin <nazmulhasanshadin000@gmail.com>
2025-10-06 11:37:14 +0600

    project folder structure has made

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,17 @@
 import app from "./app";
 
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import config from "./app/config";
 
 async function main() {
-  console.log("db url",process.env.MONGO_URI)
-  try{
+  try {
     await mongoose.connect(process.env.MONGO_URI as string);
 
-  app.listen(config.port, () => {
-    console.log(`app is listening on port ${config.port}`)
-  })
-  }
-  catch(err){
+    app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
+    });
+  } catch (err) {
     console.log(err);
   }
 }
 main();
-
-
-
